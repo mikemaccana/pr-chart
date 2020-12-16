67e9934490dbc11d245b0bb278d451427ca5b24a
Mike MacCana <mike.maccana@gmail.com>
2020-12-16 16:04:08 +0000

    Change bar chart to stack and add colors

--- src/frontend/App.svelte
+++ src/frontend/App.svelte
@@ -1,31 +1,75 @@
 <script lang="ts">
 	import type ObjectLiteral from "./object-literal"
-	export let datasets: ObjectLiteral[];
 	import Total from './Total.svelte';
 	import Chart from 'svelte-frappe-charts';
 
-  const data = {
-    labels: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
-    datasets,
-		barOptions: {
-			stacked: 1    // default 0, i.e. adjacent
+	import { http } from "./modern-http";
+
+	const log = console.log.bind(console);
+
+	let labels: string[]
+	let createdValues: string[] 
+
+	(async function () {
+		let response = await http.get("/api/v1/pull-request-summary");
+		if (response.status !== "OK") {
+			throw new Error(`Error GETting /api/v1/pull-request-summary`);
 		}
+		let prData = response.body as ObjectLiteral;
+
+		labels = Object.keys(prData.created);
+		createdValues = Object.values(prData.created);
+		log(Object.keys(prData.created));
+		log(Object.values(prData.created));
+	})();
+
+  const data = {
+    // labels,
+    labels: ["201911", "201912", "202001", "202002", "202003", "202004", "202005", "202006", "202007", "202008", "202009", "202010", "202011", "202012"],
+    datasets: [
+      {
+        name: "Created",
+        // values: createdValues,
+        values: [1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 0, 2, 2],
+        chartType: "bar",
+			},
+			{
+        name: "Updated",
+        // values: createdValues,
+        values: [1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 0, 2, 2],
+        chartType: "bar",
+      },
+      // {
+      //   name: "Dataset 2",
+      //   values: [30, 50, -10, 15, 18, 32, 27, 14],
+      //   chartType: "bar",
+      // },
+    ],
+		
 	}
 
-	enum colors {
+	enum palette {
 		brown = 'E8AA00',
 		green = '3BFF00'
 	}
+
+	let colors = [`#${palette.brown}`, `#${palette.green}`]
+
+	let barOptions = {
+		stacked: 1    // default 0, i.e. adjacent
+	}
+
 </script>
 
 <main>
 	<div class="key">
-		<Total color="{colors.brown}" name="PRs opened" count="12,123"/>
-		<Total color="{colors.green}" name="PRs closed" count="12,123"/>
+		<Total color="{palette.brown}" name="PRs opened" count="12,123"/>
+		<Total color="{palette.green}" name="PRs closed" count="12,123"/>
 	</div>
 
 	<!-- Compiler produces odd warning here: https://github.com/sveltejs/language-tools/issues/718 -->
-	<Chart {data} type="axis-mixed" />
+	<!-- See demo at https://codesandbox.io/s/frappe-charts-demo-viqud?from-embed=&file=/src/index.js for badly named 'data' variable -->
+	<Chart {data} type="axis-mixed" {colors} {barOptions}/>
 </main>
 
 <style>
--- src/frontend/main.ts
+++ src/frontend/main.ts
@@ -1,34 +1,8 @@
 import App from "./App.svelte";
 
-import { http } from "./modern-http";
-import { StatusCodes } from "./status-codes";
-
-const log = console.log.bind(console);
-
-(async function () {
-  let response = await http.get("/api/v1/pull-request-summary");
-  if (response.status !== "OK") {
-    throw new Error(`Error GETting /api/v1/pull-request-summary`);
-  }
-  log(response.body);
-})();
-
 const app = new App({
   target: document.body,
-  props: {
-    datasets: [
-      {
-        name: "Dataset 1",
-        values: [18, 40, 30, 35, 8, 52, 17, -4],
-        chartType: "bar",
-      },
-      {
-        name: "Dataset 2",
-        values: [30, 50, -10, 15, 18, 32, 27, 14],
-        chartType: "bar",
-      },
-    ],
-  },
+  props: {},
 });
 
 export default app;
